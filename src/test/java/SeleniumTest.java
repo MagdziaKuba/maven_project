b797620ba734cdeebd70af4b0f8d80cf1ef9a0a6
Magda <mkubacomperia@gmail.com>
2022-02-28 11:39:18 +0100

    chromeDriver i firefoxDriver

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,15 +1,21 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
-    public static void main(String[] args) {
-            WebDriver driver=new ChromeDriver();
 
-        }
         @Test
-        public void sampleTest(){
-            System.out.println("Hello");
+        public void openGooglePage(){
+            /*String url="C:\\Users\\mkuba\\Downloads\\chromedriver_win32\\chromedriver.exe";
+            System.setProperty("webdriver.chrome.driver",url);
+            WebDriver driver=new ChromeDriver();
+            driver.get("https://www.google.com/");*/
+
+            String url="C:\\Users\\mkuba\\Downloads\\geckodriver-v0.30.0-win64\\geckodriver.exe";
+            System.setProperty("webdriver.gecko.driver",url);
+            WebDriver driver=new FirefoxDriver();
+            driver.get("https://www.google.com/");
         }
 
 }

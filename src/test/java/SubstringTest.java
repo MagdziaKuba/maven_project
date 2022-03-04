e880f32d0d5d87feca162666e6fe029604d829e0
Magda <mkubacomperia@gmail.com>
2022-03-04 13:27:01 +0100

    lokalizowanie elementow za pomoca xpath

---
+++ src/test/java/SubstringTest.java
@@ -0,0 +1,9 @@
+public class SubstringTest {
+    public static void main(String[] args) {
+        String test="username";
+        String substring=test.substring(5);
+        //test.substring(test.length()-4);
+        //String substring=test.substring(test.length()-"ame".length());
+        System.out.println(substring);
+    }
+}
---
+++ src/test/java/XpathTest.java
@@ -0,0 +1,65 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class XpathTest {
+    @Test
+    public void findElement() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+
+        /*WebElement buttonId=driver.findElement(By.xpath("/html/body/button[@id='clickOnMe']"));
+        buttonId.click();*/
+        ////*[@id="clickOnMe"]
+        //driver.findElement(By.id("clickOnMe")).click();
+
+        WebElement nameFile=driver.findElement(By.xpath("//input[@name='fname']"));
+        nameFile.sendKeys("coś");
+
+        WebElement hiddenElement=driver.findElement(By.xpath("//p[@class='topSecret']"));
+        //znalezienie inputów na stronie
+        WebElement inputLocator=driver.findElement(By.xpath("//input"));
+        List<WebElement> inputs=driver.findElements(By.tagName("input"));
+        System.out.println(inputs.size());
+
+        WebElement linkText=driver.findElement(By.xpath("//a[text()='Visit W3Schools.com!']"));
+        //linkText.click();
+        //
+        WebElement partialLink=driver.findElement(By.xpath("//a[contains(text(),'Visit')]"));
+
+        By fullPath= By.xpath("/html/body/div/ul");
+        driver.findElement(fullPath);
+
+        By shortPth=By.xpath("//ul");
+        driver.findElement(shortPth);
+
+        By allXpath=By.xpath("//*");
+        driver.findElement(allXpath);
+
+        By secondElement= By.xpath("(//input)[2]");
+        driver.findElement(secondElement);
+
+        By lastElement=By.xpath("(//input)[last()]");
+        driver.findElement(lastElement);
+
+        By elementWithAtribut=By.xpath("//*[@name]");
+        driver.findElement(elementWithAtribut);
+
+
+        By atrEq=By.xpath("//button[@id='clickOnMe']");
+        By atrNotEq= By.xpath("//*[@id!='clickOnMe']");
+        By atrCont=By.xpath("//*[contains(@name,'ame')]");
+        By startsWith=By.xpath("//*[starts-with(@name,'user')]");
+        //By endsWith=By.xpath("//*[substring(@name,string-length(@name)-string-length('name')+1)='name']");
+
+        
+
+
+    }
+}

5274aa27ef6ae83365e02aa3a8fd30fb74bc1515
Magda <mkubacomperia@gmail.com>
2022-03-07 17:59:47 +0100

    metoda sprawdza czy znajduje sie opcja w Select

--- src/test/java/BasicActionsTest.java
+++ src/test/java/BasicActionsTest.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -11,8 +12,14 @@ import java.util.List;
 public class BasicActionsTest {
     @Test
     public void performAction(){
+
         WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        ChromeOptions options=new ChromeOptions();
+        //niewidoczny interface użytkownika
+        options.setHeadless(true);
+        WebDriver driver=new ChromeDriver(options);
+
+        //WebDriver driver=new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium");
 
         WebElement basicLink=driver.findElement(By.linkText("Podstawowa strona testowa"));
@@ -32,6 +39,10 @@ public class BasicActionsTest {
         usernameInput.clear();
         usernameInput.sendKeys("admin");
         //usernameInput.sendKeys(Keys.TAB);
+
+        //System.out.println(usernameInput.getText());- nie zadziała
+        //Pobieranie wartości z pola tekstowego
+        System.out.println(usernameInput.getAttribute("value"));
 
         usernameInput.sendKeys(Keys.ENTER);
 
@@ -59,9 +70,14 @@ public class BasicActionsTest {
         System.out.println(checkSelect.isOptions("Saab",selectCar));
         System.out.println(checkSelect.isOptions("Cos",selectCar));
 
+//pobieranie wartości z ukrytego elementu->getAttribute("textContent")
+        WebElement paragraf= driver.findElement(By.cssSelector(".topSecret"));
+        System.out.println("getText "+paragraf.getText());
+        System.out.println("getAttribute value "+paragraf.getAttribute("value"));
+        System.out.println("getAttribute textContent "+paragraf.getAttribute("textContent"));
 
 
-
+        //https://www.w3schools.com/js/js_popup.asp
 
 
 

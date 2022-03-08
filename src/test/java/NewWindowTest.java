e24c11d2306e50c003972eef40746a644a8e665a
Magda <mkubacomperia@gmail.com>
2022-03-08 11:34:05 +0100

    przepinanie się między okienkami

---
+++ src/test/java/NewWindowTest.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
+public class NewWindowTest {
+    @Test
+    public void testNewWindow(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        //przepinanie się do okien
+        String currentWindow=driver.getWindowHandle();
+        driver.findElement(By.id("newPage")).click();
+        Set<String> windowNames=driver.getWindowHandles();
+        for(String window : windowNames){
+            if(!window.equals(currentWindow)){
+                driver.switchTo().window(window);
+            }
+        }
+        WebElement agreeButton=driver.findElement(By.xpath("//div[contains(text(),'Zgadzam')]"));
+        agreeButton.click();
+        driver.findElement(By.name("q")).sendKeys("Selenium");
+        driver.switchTo().window(currentWindow);
+        driver.findElement(By.name("fname")).sendKeys("Magda");
+    }
+}

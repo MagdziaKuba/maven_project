4f38ff8e03c6d7ae0b60566614971f8fa2345d0e
Magda <mkubacomperia@gmail.com>
2022-03-08 13:59:27 +0100

    screenshot

--- src/test/java/UploadTest.java
+++ src/test/java/UploadTest.java
@@ -1,15 +1,25 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class UploadTest {
     @Test
-    public void uploadFile(){
+    public void uploadFile() throws IOException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
         driver.findElement(By.id("myFile")).sendKeys("C:\\Users\\mkuba\\Documents\\naukaMoja\\selenium\\simple.txt");
+
+        TakesScreenshot screenshot=(TakesScreenshot) driver;
+        File src=screenshot.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src, new File("src/test/resources/screenshot.png"));
     }
 }

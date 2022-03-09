4fda2fe6a239b8b72c4f2a06bb0e68af1019d9f7
Magda <mkubacomperia@gmail.com>
2022-03-09 12:55:37 +0100

    klikniecie i podwojne klikniecie

--- src/test/java/ActionsTest.java
+++ src/test/java/ActionsTest.java
@@ -11,31 +11,23 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.io.IOException;
 
-public class UploadTest {
+public class ActionsTest {
     @Test
     public void uploadFile() throws IOException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
-
-        int randomNumber=(int) (Math.random()*1000);
-
-        driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
-        driver.findElement(By.id("myFile")).sendKeys("C:\\Users\\mkuba\\Documents\\naukaMoja\\selenium\\simple.txt");
-
-        TakesScreenshot screenshot=(TakesScreenshot) driver;
-        File src=screenshot.getScreenshotAs(OutputType.FILE);
-        String nameFile="screenshot"+randomNumber+".png";
-        FileUtils.copyFile(src, new File("src/test/resources/"+nameFile));
+        driver.get("https://testeroprogramowania.github.io/selenium/doubleclick.html");
 
         //klikanie prawym przyciskiem myszy
         Actions actions=new Actions(driver);
-        actions.contextClick().perform();
+        //actions.contextClick().perform();
 
-        //podwójne kliknięcie
+
         //actions.contextClick(driver.findElement(By.id("myFile"))).perform();
         //perform()-wykonanie sie
-
-
+        //podwójne klikniecie
+        actions.doubleClick(driver.findElement(By.id("bottom"))).perform();
+        
 
     }
 }

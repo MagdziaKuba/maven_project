cdfef7be4779909fa1e09f4cc2fa0660f8e00d6a
Magda <mkubacomperia@gmail.com>
2022-03-10 10:57:47 +0100

    czy img się załadował

--- src/test/java/ElementExistTest.java
+++ src/test/java/ElementExistTest.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -18,6 +19,16 @@ public class ElementExistTest {
         System.out.println(elementExistSecond(By.tagName("p")));
         System.out.println(elementExistSecond(By.id("topSecret")));
 
+        //czy wyświetlony
+        System.out.println(driver.findElement(By.tagName("p")).isDisplayed());
+
+        //czy możemy wejść w interakcję z elementem
+        System.out.println(driver.findElement(By.tagName("p")).isEnabled());
+
+        WebElement checkbox=driver.findElement(By.cssSelector("[type='checkbox']"));
+        System.out.println("Czy checkbox jest zaznaczony? "+checkbox.isSelected());
+        checkbox.click();
+        System.out.println("Czy checkbox jest zaznaczony? "+checkbox.isSelected());
     }
     public boolean elementExist(By locator){
         try {
---
+++ src/test/java/ImageTest.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ImageTest {
+    @Test
+    public void imageExistTest(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/image.html");
+        //driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        WebElement image=driver.findElement(By.tagName("img"));
+        //wysokość obrazka
+        //jak 0 to nie wyświetlił się poprawnie
+        System.out.println(image.getAttribute("naturalHeight"));
+        //https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/naturalHeight
+        
+        /*String height=image.getAttribute("naturalHeight");
+        Assert.assertEquals(height,"0");*/
+
+    }
+}

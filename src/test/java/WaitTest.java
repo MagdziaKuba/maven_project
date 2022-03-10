aaf6e7631f7aeda936f6a5ab8cecf60779e65a6a
Magda <mkubacomperia@gmail.com>
2022-03-10 13:07:04 +0100

    czekajka FluentWait

--- src/test/java/WaitTest.java
+++ src/test/java/WaitTest.java
@@ -1,19 +1,39 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class WaitTest {
     @Test
-    public void waittest() throws InterruptedException {
+    public void waittest() /*throws InterruptedException*/ {
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/wait2.html");
+        //przez 10 sekund zadaje pytanie, aż do pojawienia się
+        //max 10 sek, min 0
+        //wykład 203 przestarzałe, nie działą
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.findElement(By.id("clickOnMe")).click();
         //ostateczność
-        Thread.sleep(5000);
+        //Thread.sleep(5000);
+
+        /*WebDriverWait wait=new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p")));*/
+
+        FluentWait<WebDriver> wait= new FluentWait<>(driver);
+        wait.ignoring(NoSuchElementException.class);
+        wait.withTimeout(Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p")));
+
         driver.findElements(By.cssSelector("p"));
     }
 }

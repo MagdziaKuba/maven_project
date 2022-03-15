4593e23a4bb059569138e8374ca756a42ca4b745
Magda <mkubacomperia@gmail.com>
2022-03-15 09:24:21 +0100

    @BeforeSuite, @BeforeClass, @AfterSuite, @AfterClass

--- src/test/java/testNG/BaseTest.java
+++ src/test/java/testNG/BaseTest.java
@@ -1,12 +1,28 @@
 package testNG;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 
 public class BaseTest {
     //jest rodzicem dla FirstTest i SecondTest
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("I am runing before suite.");
+    }
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("I am runing before class.");
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("I am runing after suite.");
+    }
+    @AfterClass
+    public void afterClass(){
+        System.out.println("I am runing after class.");
+    }
+
     @BeforeTest
     public void beforeTest(){
         System.out.println("I am runing before test.");

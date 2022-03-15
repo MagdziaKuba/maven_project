9ace5fcf584acd26ff485ae874dfd6150a0a8f5b
Magda <mkubacomperia@gmail.com>
2022-03-15 09:15:05 +0100

    @AfterTest, @AfterMethod

--- src/test/java/testNG/BaseTest.java
+++ src/test/java/testNG/BaseTest.java
@@ -1,5 +1,7 @@
 package testNG;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
@@ -12,5 +14,15 @@ public class BaseTest {
     @BeforeMethod
     public void beforeMethod(){
         System.out.println("I am runing before method.");
+    }
+
+    @AfterTest
+    public void afterTest(){
+        System.out.println("I am runing after test.");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("I am runing after method.");
     }
 }
